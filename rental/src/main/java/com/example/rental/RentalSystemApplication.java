87ffbc01391144ff4fa5d946d8a066392013533f
Athindu <athinduama@gmail.com>
2020-07-31 00:35:45 +0530

    Updated

---
+++ rental/src/main/java/com/example/rental/RentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalSystemApplication.class, args);
+	}
+
+}
---
+++ rental/src/test/java/com/example/rental/RentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
