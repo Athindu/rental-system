f9091ef81352b1caf9a514be968f8dd2c887f2f7
Athindu <athinduama@gmail.com>
2020-07-31 13:23:55 +0530

    backend updated

---
+++ rentalApp/src/test/java/com/example/rentalApp/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
